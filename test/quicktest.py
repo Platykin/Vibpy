5f817527bf97d34296d0f74bd53638a7e0ac7254
Platykin <igor.alves.pinto11@gmail.com>
2024-07-15 11:16:01 -0300

    Add documentation for num_sample_per_interval and fix the plot problem

--- test/quicktest.py
+++ test/quicktest.py
@@ -58,14 +58,17 @@ def read_data(file_path):
         raise ValueError("Unsupported file format")
     return data
 
-def extract_fft_components(filtered_fft):
+def extract_fft_components(filtered_fft, nyquist_frequencie):
     frequencies = []
     magnitudes = []
     
     for freq, complex_amp in filtered_fft:
-        frequencies.append(freq)
-        magnitude = np.sqrt(complex_amp.real**2 + complex_amp.imag**2)
-        magnitudes.append(magnitude)
+        if freq >= 0:
+            frequencies.append(freq)
+            magnitude = np.sqrt(complex_amp.real**2 + complex_amp.imag**2)
+            if freq > 0 and freq < nyquist_frequencie:
+                magnitude *= 2
+            magnitudes.append(magnitude)
     
     return frequencies, magnitudes
 
@@ -94,17 +97,45 @@ data = read_data(file_path)
 
 # Still to add
 # sampling_rate = 800  # Hz
-# t = 60 * sampling_rate  # Número de amostras por intervalo
-# res = 1 / sampling_rate  # Intervalo de tempo entre amostras
+# t = sampling_rate  # Número de amostras por intervalo
+# ex: Caso tenhamos uma taxa de amostragem de 1000 Hz, se t = 1000, estaremos analisando 1000 amostras no intervalo de 1 segundo
+# Mas poderíamos fazer também t = 500, que seriam 1000 amostras para cada 0,5 segundo
+# Ou também t = 2000, que seriam 1000 amostras para cada 2 segundos 
+
+#  ---------------------------------------------------------------------------------------------------------------------------------------------------
+# | t Maior -> Melhor resolução de frequência, menor resolução temporal.                                                                              |
+# | t Menor -> Melhor resolução temporal, menor resolução de frequência.                                                                              |
+# |                                                                                                                                                   |
+# | Sinal Estacionário: Se o sinal não muda muito ao longo do tempo, um t maior pode ser usado para obter uma melhor resolução de frequência.         |
+# |       Análise de Vibração em Máquinas:                                                                                                            |
+# |          Se você está analisando a vibração de uma máquina que opera de forma relativamente estável, você pode escolher um t maior para ter       |
+# |        uma resolução de frequência mais precisa.                                                                                                  |
+# |                                                                                                                                                   |
+# |      Exemplo: t = 2000 para intervalos de 2 segundos (taxa de amostragem = 1000 Hz).                                                              |
+# |                                                                                                                                                   |
+# | Sinal Não Estacionário: Se o sinal muda rapidamente, um t menor é preferível para capturar essas mudanças com maior resolução temporal.           |
+# |       Análise de Sinais Biológicos:                                                                                                               |
+# |          Para sinais biológicos, como a atividade cerebral, onde as mudanças podem ser rápidas, um t menor pode ser mais apropriado.              |
+# |                                                                                                                                                   |
+# |          Exemplo: t = 500 para intervalos de 0,5 segundo (taxa de amostragem = 1000 Hz).                                                          |
+#  ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+# res = 1 / sampling_rate  # Intervalo de tempo entre amostras, inverso da taxa de amostragem
 # fMin = 0.1
 # fMax = 50
 
+sampling_rate = 800
+num_samples_per_interval = 2 * sampling_rate
+sampling_interval = 1/sampling_rate
+nyquist_frequencie = sampling_rate/2
 
-resultados = VibData(t=5000, res=0.1, fMin=0.1, fMax=15, data=data)
+
+
+resultados = VibData(t=num_samples_per_interval, res=sampling_interval, fMin=0.1, fMax=15, data=data)
 
 # print(resultados)
 
-frequencias, magnitudes = extract_fft_components(resultados[0][3])
+frequencias, magnitudes = extract_fft_components(resultados[0][3], nyquist_frequencie)
 
 # for i in range(20):
 #     print(frequencias[i], "\t", magnitudes[i])
